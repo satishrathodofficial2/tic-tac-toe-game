1129a919392c502fec33656aab3bd0db67d97b56
satishrathodofficial2 <satishrathodofficial2@gmail.com>
2024-05-20 23:51:18 +0530

    added x and y turn

--- main.py
+++ main.py
@@ -20,6 +20,23 @@ def printBoard(xState, zState):
     print(f"--|---|---")
     print(f"{six} | {seven} | {eight} ")
 
+
+
+# def checkWin(xState, zState):
+#     wins = [[0,1,2], [3,4,5], [6, 7, 8,], [0, 3, 6], [1, 4, 7,], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+#     for win in wins:
+#         if(sum(xState[win[0]]), xState[win[1]], xState[win[2]]):
+#             print("X Won the match")
+#             return 1
+#         if(sum(zState[win[0]]), zState[win[1]], zState[win[2]]):
+#             print("O Won the match")
+#             return 0
+#         return - 1
+        
+    
+#     if(sum(i) == 3):
+#         return 1
+
 if __name__ == "__main__":
     xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     zState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -35,5 +52,5 @@ if __name__ == "__main__":
             print("O's Chance")
             value = int(input("please enter a value: "))
             zState[value] = 1
-
+        # checkWin(xState, zState)
         turn = 1-turn
